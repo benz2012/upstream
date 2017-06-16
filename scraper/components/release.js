2a83d6d9a0c90af21e838b91e879f1b2b5a5cf4b
Ben Zenker <imbenzenker@gmail.com>
2017-06-16 15:24:41 -0400

    added release filters and mappings to ensure that only valid releases get pushed to the database

--- scraper/components/release.js
+++ scraper/components/release.js
@@ -5,6 +5,19 @@ class Release {
     this.service = service
     this.releaseType = releaseType
   }
+
+  valid() {
+    const attributes = Object.keys(this).map(key => this[key])
+    return attributes.every(valid_attribute)
+  }
+  worthy() {
+    // return false if release is not a tv show with an identifiable release date
+    return true
+  }
 }
+
+const valid_string = (val) => (typeof val === 'string' || val instanceof String)
+const lengthy = (val) => (val.length > 0)
+const valid_attribute = (val) => (valid_string(val) && lengthy(val))
 
 module.exports = Release
--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,6 +1,7 @@
 const netflixScrape = require('./services/netflix.js')
 const releaseURLs = require('./release_urls.json')
 
+// Scrape data from all URLs
 const scrapePromises = Object.keys(releaseURLs).map(service => {
   const url = releaseURLs[service]
   return new Promise(resolve => {
@@ -16,16 +17,30 @@ const scrapePromises = Object.keys(releaseURLs).map(service => {
   })
 })
 
-Promise.all(scrapePromises).then(resolutions => {
-  const allReleases = []
-  resolutions.forEach(resolution => {
-    if (resolution) {
-      resolution.forEach(release => {
-        allReleases.push(release)
-      })
+// Mangle data into worthwhile release objects and send those to the database
+Promise.all(scrapePromises)
+.then(resolutions => flatten(resolutions))
+.then(releases => {
+  releases.forEach(r => {
+    if (!r.valid()) {
+      if (r.service) {
+        console.log(r.service + " service scraper has failed to scrape a release")
+      }
     }
   })
-  return allReleases
-}).then(releases => {
-  console.log(releases)
+  return releases.filter(r => r.valid())
 })
+.then(releases => releases.filter(r => r.worthy()))
+.then(releases => {
+  console.log(releases)
+  // send these to Firebase
+})
+
+
+// Utility Functions
+const flatten = (arr) => arr.reduce(
+  (acc, val) => acc.concat(
+    Array.isArray(val) ? flatten(val) : val
+  ),
+  []
+)
