834886d99f8e08c58d8d59000936f9d90f69cf3a
Ben Zenker <imbenzenker@gmail.com>
2017-06-19 01:33:05 -0400

    initial logic flow

--- skill/index.js
+++ skill/index.js
@@ -4,45 +4,19 @@ const moment = require('moment')
 
 const APP_ID = 'amzn1.ask.skill.20b56d1a-0616-4c72-90d5-34a061f0b873'
 const SKILL_NAME = 'Upstream'
-const HELP_MESSAGE = "You can say what's coming out this week, or, you can say exit."
+const HELP_MESSAGE = "You can say, what's coming out this week, or, you can say exit. What can I help you with?"
 const HELP_REPROMPT = 'What can I help you with?'
 const STOP_MESSAGE = 'Goodbye!'
 
-const releases = {
-  "00d38e6a-acb7-4e30-a82d-99b67f992d5f": {
-    "date": "2017-06-23T04:00:00.000Z",
-    "name": "GLOW",
-    "service": "Netlfix"
-  },
-  "00f35edc-2411-4fd3-99c1-307ec4c96782": {
-    "date": "2017-07-07T04:00:00.000Z",
-    "name": "Castlevania",
-    "service": "Netlfix"
-  },
-  "05ee20a4-2b1f-47fa-b780-e67f6b934b7a": {
-    "date": "2017-07-14T04:00:00.000Z",
-    "name": "Friends From College",
-    "service": "Netlfix"
-  },
-  "8bf242fa-b614-41a0-a9bc-0d1a8c666dbc": {
-    "date": "2017-06-30T04:00:00.000Z",
-    "name": "Gypsy",
-    "service": "Netlfix"
-  },
-  "b4c90dc8-5804-4fe3-b729-e16a77c15ba4": {
-    "date": "2017-06-16T04:00:00.000Z",
-    "name": "El Chapo",
-    "service": "Netlfix"
-  },
-  "bc254d7c-4345-44ba-8d72-f1f9b9697acd": {
-    "date": "2017-06-16T04:00:00.000Z",
-    "name": "The Ranch",
-    "service": "Netlfix"
-  },
-  "d50a6e34-7015-4128-8b38-cd8c90893aa0": {
-    "date": "2017-07-21T04:00:00.000Z",
-    "name": "Ozark",
-    "service": "Netlfix"
+const localReleases = {
+  "netflix": {
+    "GLOW": "2017-06-23T04:00:00.000Z",
+    "Castlevania": "2017-07-07T04:00:00.000Z",
+    "Friends From College": "2017-07-14T04:00:00.000Z",
+    "Gypsy": "2017-06-30T04:00:00.000Z",
+    "El Chapo": "2017-06-16T04:00:00.000Z",
+    "The Ranch": "2017-06-16T04:00:00.000Z",
+    "Ozark": "2017-07-21T04:00:00.000Z"
   }
 }
 
@@ -56,10 +30,70 @@ exports.handler = function(event, context, callback) {
 const handlers = {
   'LaunchRequest': function () { this.emit('GetReleases') },
   'GetReleases': function () {
+    let speechOutput = ''
     const { slots } = this.event.request.intent
-    const slotVals = Object.keys(slots).map(slot => slots[slot].value)
-    const slotList = slotVals.join(', ')
-    this.emit(':tell', slotList)
+
+    let timeMoment = moment().week()
+    let timeFilter = 'week'
+
+    if (slots.TimePeriod.value) {
+      timeMoment = moment(slots.TimePeriod.value)
+    }
+    if (!timeMoment.isValid()) {
+      askDateInvalid()
+    } else {
+      // Date is Vaild, determine what type of date user gave
+      const timeFormat = timeMoment._f
+      if (!timeFormat) {
+        if (timeMoment._i.length === 4 && parseInt(timeMoment)) {
+          // Date was given as a year
+          timeFilter = 'year'
+        }
+      } else {
+        askDateInvalid()
+      } else {
+        if (rightTwo(timeFormat) === 'MM') {
+          // Date was given as a month
+          timeFilter = 'month'
+        } else if (rightTwo(timeFormat) === 'WW') {
+          // Date was given as a week
+          timeFilter = 'week'
+        } else if (rightTwo(timeFormat) === 'DD') {
+          // Date was given as a day
+          timeFilter = 'day'
+        }
+      }
+    }
+    const timeBeginFilter = timeMoment.startOf(timeFilter)
+    const timeEndFilter = timeMoment.endOf(timeFilter)
+
+    let releaseResults = {}
+    if (slots.Service.value) {
+      releaseResults = filterReleasesOfService(
+        localReleases, timeBeginFilter, timeEndFilter, slots.Service.value
+      )
+      if (releaseResults.length < 1) {
+        askNoReleasesForTime()
+      } else {
+        tellReleases(releaseResults)
+      }
+    } else {
+      releaseResults = filterReleases(
+        localReleases, timeBeginFilter, timeEndFilter
+      )
+      if (releaseResults.length < 1) {
+        askNoReleasesForTime()
+      }
+      if (!slots.NumberToRead.value) {
+        tellReleasesAskForNumber(
+          releaseResults.length, timeMoment.format(), this.event.request.intent
+        )
+      } else {
+        tellReleases(
+          releaseResults.slice(0, slots.NumberToRead.value)
+        )
+      }
+    }
   },
   'GetReleaseDate': function () {
     const { slots } = this.event.request.intent
@@ -74,4 +108,50 @@ const handlers = {
   },
   'AMAZON.CancelIntent': function () { this.emit(':tell', STOP_MESSAGE) },
   'AMAZON.StopIntent': function () { this.emit(':tell', STOP_MESSAGE) }
+}
+
+// ALEXA SPEACH EMITTERS
+const askDateInvalid = function() {
+  const speechOutput = 'Date Invalid. Ask again.'
+  this.emit(':ask', speechOutput, speechOutput)
+}
+const askNoReleasesForTime = function() {
+  const speechOutput = 'There are no releases for that time period. You can ask for new releases for a different time period.'
+  const repromptSpeech = 'You can ask for new releases for a different time period.'
+  this.emit(':ask', speechOutput, repromptSpeech)
+}
+const tellReleasesAskForNumber = function(number, timePeriod, originalIntent) {
+  const speechOutput = `There are ${number} new releases ${timePeriod}. How many would you like to hear?`
+  const repromptSpeech = `How many of ${timePeriod}'s new releases would you like to hear?`
+  this.emit(':elicitSlot', 'NumberToRead', speechOutput, repromptSpeech, originalIntent)
+}
+const tellReleases = function(releases) {
+  const speechOutput = Object.keys(releases).join(', ')
+  this.emit(':tell', speechOutput)
+}
+
+// UTILITY FUNCTIONS
+const rightTwo = (str) => str.substring(str.length - 2)
+
+const filterReleases(releases, start, end) => {
+  const filtered = []
+  Object.keys(releases).forEach(service => {
+    filtered.concat(filterReleasesOfService(releases, start, end, service))
+  })
+  return filtered
+}
+
+const filterReleasesOfService = (releases, start, end, service) => {
+  const focus = releases[service]
+  const filtered = Object.keys(focus)
+    .filter(series => moment(focus[series]).isBetween(start, end, null, '[]'))
+    .sort((a, b) => {
+      const compare = moment(focus[a]).isSameOrBefore(focus[b])
+      return (!compare * -1) + (compare * 1) // 1 for true, -1 for false
+    })
+    .reduce((obj, key) => {
+      obj[key] = focus[key]
+      return obj
+    }, [])
+  return filtered
 }
